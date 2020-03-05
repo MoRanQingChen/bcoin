beed6216550feac4f545d15e537f0aeba4353781
jiangpeng <jiangpeng@kedacom.com>
2020-03-05 10:29:42 +0800

    增加内容

--- app.js
+++ app.js
@@ -24,22 +24,41 @@ http.createServer(function(req,res){
             .set({'Usertempid':''})
             .set({'Host':'blz.bicoin.com.cn'})
             .end((err,response)=>{
-                
+                let content = ''
                 let info = response.body.data;
-                res.setHeader('Content-Type','text/html;charset=utf-8');
-                fs.readFile('./index.html','utf8',function(err,data){
-                    let html = data.toString().replace('<!-- content -->',`${
-                        info.filter(item=>item.pointContent).map(item=>{
-                            return `<section class="item ${/免费/.test(item.statusStr)?'free':''}">
+                superagent.get('http://112.124.201.164:9000/circle/getCircleContent?createUserId=5333')
+                .end((err,response2)=>{
+                    let info2 = response2.body.data.threadList.filter((item,index,arr)=>item.dateline*1>(arr[index+1]||{dateline:0}).dateline*1)
+                    content += `<section class="item">
+                    <div class="header">
+                        <img src="http://topcoin.oss-cn-hangzhou.aliyuncs.com/lr_headimg/75fac2373382ab813b397915e1ff1b29.JPEG">
+                        <div>
+                            <div>南砚</div>
+                            <span class="time">vx:jump_with_joy</span>
+                        </div>
+                    </div>
+                    ${info2.map((item,index)=>{
+                        let date = getTimeDiff(new Date(item.dateline*1000));
+                        return `
+                            <div class="contain ${index==0?'':'border_top'}">
+                                ${item.content.replace(/\n/g,'</br>').replace(/^<\/br>/,'')}
+                                <div style="text-align:right;color: #3064d8;" class="time">${date}</div>
+                            </div>`
+                        }).join('\n')
+                    }
+                    </section>`
+                    content += `${
+                        info.filter(item=>!/半木夏/.test(item.leaderName)&&item.pointContent).map(item=>{
+                            return `<section class="item ${/免费/.test(item.statusStr)?'freeX':''}">
                                 <div class="header">
                                     <img src="${item.img}">
                                     <div>
                                         <div>${item.leaderName}</div>
-                                        <a href="${item.pointUrl}">${item.pointTimeStr.split(' ')[0]}</a>
+                                        <a class="time" href="${item.pointUrl}">${item.pointTimeStr.split(' ')[0]}</a>
                                     </div>
                                 </div>
                                 <div class="contain">
-                                    内容 ${item.pointContent.replace(/\n/g,'</br>')}
+                                    ${item.pointContent.replace(/\n/g,'</br>')}
                                 </div>
                                 <ul class="info">
                                     <li class="${item.balance<0?'red':''}">${item.balance}$<span>资金</span></li>
@@ -49,10 +68,15 @@ http.createServer(function(req,res){
                                 </ul>
                             </section>`
                         }).join('\n')
-                    }`)
-                    res.write(html)
-                    res.end();
+                    }`
+                    res.setHeader('Content-Type','text/html;charset=utf-8');
+                    fs.readFile('./index.html','utf8',function(err,data){
+                        let html = data.toString().replace('<!-- content -->',content)
+                        res.write(html)
+                        res.end();
+                    })
                 })
+                
             })
         
     }else{
@@ -62,5 +86,16 @@ http.createServer(function(req,res){
     }
 }).listen(8090);
 
-
+function getTimeDiff(date){
+    timeold = (Date.now() - date.getTime()); //总豪秒数
+    secondsold = Math.floor(timeold / 1000);          //总秒数
+    e_daysold = timeold / (24 * 60 * 60 * 1000);
+    daysold = Math.floor(e_daysold);                 //相差天数
+    e_hrsold = (e_daysold - daysold) * 24;
+    hrsold = Math.floor(e_hrsold);                   //相差小时数
+    e_minsold = (e_hrsold - hrsold) * 60;
+    minsold= Math.floor(e_minsold)                   //相差分钟数
+    seconds = Math.floor((e_minsold - minsold) * 60);  //相差秒数
+    return (daysold!=0?(daysold + "天"):'') + (hrsold!=0?(hrsold + "小时"):'') + (minsold!=0?(minsold + "分"):'') + seconds + "秒前"
+}
 
